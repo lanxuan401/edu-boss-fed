ecbf4ee0c1fb75f0db16434d5d8c1c527b4b028d
llf <lanxuanvip163@163.com>
2022-06-21 00:05:01 +0800

    角色管理

--- src/services/menu.ts
+++ src/services/menu.ts
@@ -32,3 +32,29 @@ export const deleteMenu = (id: number) => {
     url: `/boss/menu/${id}`
   })
 }
+
+export const getMenuNodeList = () => {
+  return request({
+    method: 'GET',
+    url: '/boss/menu/getMenuNodeList'
+  })
+}
+
+export const allocateRoleMenus = (data:any) => {
+  return request({
+    method: 'POST',
+    url: '/boss/menu/allocateRoleMenus',
+    data
+  })
+}
+
+export const getRoleMenus = (roleId: string | number) => {
+  return request({
+    method: 'GET',
+    url: '/boss/menu/getRoleMenus',
+    params: {
+      // axios 会把params 转换为key=value&key=value的数据格式放到url后面
+      roleId
+    }
+  })
+}
